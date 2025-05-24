f2e08f294b5640dc9044a2c356c504c3744b8e60
Rostyslav Futornyi <ros@Rostyslavs-MacBook-Pro.local>
2025-05-24 21:33:07 +0300

    feat: add posthog

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Montserrat } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import { PostHogProvider } from "./components/PostHogProvider";
 
 const montserrat = Montserrat({
   subsets: ['latin', 'cyrillic'],
@@ -28,33 +29,35 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru" className={montserrat.variable}>
       <body>
-        {/* Meta Pixel Code */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '553830737785923');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=553830737785923&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
-        <Navigation />
-        {children}
+        <PostHogProvider>
+          {/* Meta Pixel Code */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '553830737785923');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=553830737785923&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+          {/* End Meta Pixel Code */}
+          <Navigation />
+          {children}
+        </PostHogProvider>
       </body>
     </html>
   );
