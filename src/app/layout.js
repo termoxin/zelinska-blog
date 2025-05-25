f99ca2321fb6d2e98e537c508a4e2ffb7731417f
Rostyslav Futornyi <ros@Rostyslavs-MacBook-Pro.local>
2025-05-25 16:15:23 +0300

    fix: favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,13 +23,14 @@ export const metadata = {
   description: "Психолог для тревожных людей. Работа с тревожностью, ОКР, паническими атаками.",
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon.ico', sizes: 'any' }
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ]
+    ],
+    shortcut: '/favicon.ico'
   },
   manifest: '/site.webmanifest',
   other: {
